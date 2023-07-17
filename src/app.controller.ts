2962dfc25826eb46a4ce9f7e2cd741b5cc2ab3bd
ionut <sandaionutflorin@yahoo.com>
2023-07-17 17:21:30 +0300

    basic pm2 server with 4 endpoints

---
+++ src/app.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { AppService } from './app.service';
+import * as pm2 from 'pm2';
+
+//pm2 processes must be started from user ionut!!!
+
+@Controller()
+export class AppController {
+  public constructor(private readonly appService: AppService) {}
+
+  @Get("/pm2/list")
+  getPm2ServicesListController(){
+    return this.appService.getPm2ServicesList();
+  }
+
+  @Get("/pm2/:id/restart")
+  restartPm2ServiceController(@Param("id") id: number){
+    return this.appService.restartPm2Service(id);
+  }
+
+  @Get("/pm2/:id/start")
+  startPm2ServiceController(@Param("id") id: string){
+    return this.appService.startPm2Service(id);
+  }
+
+  @Get("/pm2/:id/stop")
+  stopPm2ServiceController(@Param("id") id: string){
+    return this.appService.stopPm2Service(id);
+  }
+
+}
---
+++ src/app.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@nestjs/common';
+import * as pm2 from 'pm2';
+
+@Injectable()
+export class AppService {
+
+  getPm2ServicesList(): Promise<pm2.ProcessDescription[]> {
+    return new Promise((resolve, reject) => {
+      pm2.list((err, list) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(list);
+        }
+      })
+    })
+  }
+
+  //id can either be the id of the process or the name of the process
+  restartPm2Service(idOrName: number | string) {
+    return new Promise((resolve, reject) => {
+      pm2.restart(idOrName, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+            resolve({ success: `Process with ID: ${idOrName} was restarted!` });
+        }
+      })
+    })
+  }
+
+  startPm2Service(idOrName: string) {
+    return new Promise((resolve, reject) => {
+      pm2.start(idOrName, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve({ success: `Process with ID: ${idOrName} was started!` });
+        }
+      })
+    })
+  }
+
+  stopPm2Service(idOrName: string) {
+    return new Promise((resolve, reject) => {
+      pm2.stop(idOrName, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve({ success: `Process with ID: ${idOrName} was stopped!` });
+        }
+      })
+    })
+  }
+
+}
